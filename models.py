b45f4511054f65130904bc4fe6d8d2776b1f49e9
tram-m-nguyen <tn334@cornell.edu>
2020-11-12 20:23:56 -0500

    finished setting up db, wrote script to generate data

--- models.py
+++ models.py
@@ -29,14 +29,17 @@ class BallotSearch(db.Model):
     def __repr__(self):
       """Return representation of search instance."""
 
-        return f"<BallotSearch year: {self.year}, month: {self.month}, \
-                subject: {self.subject}, type_of_measure: {self.type_of_measure}, \
-                measure_placed_by: {measure_placed_by}, pass_of_fail: {self.pass_of_fail},\
+      return f"<BallotSearch year: {self.year}, month: {self.month}, \n \
+                subject: {self.subject}, type_of_measure: {self.type_of_measure}, \n \
+                measure_placed_by: {self.measure_placed_on_ballot_by}, \n \
+                pass_of_fail: {self.pass_or_fail},\n \
                 keyword: {self.keyword} >"
+    
+    
 
 
 
-class BallotFromMainDatabase(db.Model):
+class BallotsFromMainDB(db.Model):
     """
     This is the main ballot database from SF library.
     Date: November 1907- March 2020.
@@ -46,43 +49,67 @@ class BallotFromMainDatabase(db.Model):
     outside range of 1907-1960 and 2011-2020.
 
     From the excel spreadsheet, columns prop_letter, pass_for_fail, vote_counts, 
-    and measure_placed_on_ballot_by have blanks, so these columns will have nullable=True
+    and measure_placed_on_ballot_by have blanks, percent, percent_required_to_pass,
+    so these columns will have nullable=True
 
-    pdf_available: tells if there's a pdf voter pamphet available, else - noimage
+    pdf_available: tells if there's a pdf voter pamphlet available, else - noimage
     """
 
     __tablename__ = "ballots"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    month = db.Column(db.Text, nullable=False)
+    month = db.Column(db.Text, nullable=True)
     day = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String, nullable=False)
     prop_letter = db.Column(db.Text, nullable=True)
-    subject = db.Column(db.String, nullable=False) #subject from API
-    type_of_measure = db.column(db.Text, nullable=False)
+    ballot_subject = db.Column(db.String, nullable=True) 
+    type_of_measure = db.Column(db.Text, nullable=False)
     measure_placed_on_ballot_by = db.Column(db.Text, nullable=True)
     description = db.Column(db.String, nullable=False)
     pass_or_fail = db.Column(db.String, nullable=True)
     vote_counts = db.Column(db.String, nullable=True)
-    percent = db.Column(db.String, nullable=False)
-    percent_required_to_pass = db.Column(db.String, nullable=False)
+    percent = db.Column(db.String, nullable=True)
+    percent_required_to_pass = db.Column(db.String, nullable=True)
     pdf_available = db.Column(db.String, nullable=False)
   
     def __repr__(self):
         """Returns a representation of ballot instance."""
 
-        return f"<BallotFromMainDatabase year: {self.year}, month: {self.month}, \
-                prop_letter: {self.prop_letter}, subject: {self.subject}, 
-                type_of_measure: {self.type_of_measure}, \
-                measure_placed_on_ballot_by: {measure_placed_by}, \
-                description: {self.description}, pass_of_fail: {self.pass_of_fail},\
-                votes_counts: {self.vote_counts}, percent: {self.percent}, \
-                percent_required_to_pass: {self.percent_required_to_pass}, \
+        return f"<BallotFromMainDatabase year: {self.year}, month: {self.month}, \n \
+                prop_letter: {self.prop_letter}, subject: {self.ballot_subject}, \n \
+                type_of_measure: {self.type_of_measure}, \n \
+                measure_placed_on_ballot_by: {self.measure_placed_on_ballot_by}, \n \
+                description: {self.description}, pass_of_fail: {self.pass_or_fail}, \n\
+                votes_counts: {self.vote_counts}, percent: {self.percent}, \n \
+                percent_required_to_pass: {self.percent_required_to_pass}, \n \
                 pdf_avail: {self.pdf_available} >"
   
    
-  
+    # def serialize(self):
+    #     """Serialize ballot response from database to a dictionary."""
+
+    #     return {
+    #         "id": self.id,
+    #         "month": self.month,
+    #         "day": self.day, 
+    #         "year": self.year,
+    #         "date": self.date,
+    #         "prop_letter": self.prop_letter,
+    #         "ballot_subject": self.ballot_subject,
+    #         "type_of_measure": self.type_of_measure,
+    #         "measure_placed_on_ballot_by": self.measure_placed_on_ballot_by,
+    #         "description": self.description,
+    #         "pass_or_fail": self.pass_or_fail,
+    #         "vote_counts": self.vote_counts,
+    #         "percent": self.percent,
+    #         "percent_required_to_pass": self.percent_required_to_pass,
+    #         "pdf_available": self.pdf_available
+    #         }
+
+
+
+
 # old dataset before cleaning excel spreadsheet
 
 #   compare api to main excel spreasheet
---
+++ seed.py
@@ -0,0 +1,18 @@
+"""Seed database with real and sample data from CSV Files."""
+
+from csv import DictReader 
+from app import db  
+from models import BallotSearch, BallotsFromMainDB
+
+db.drop_all()
+db.create_all()
+
+# map ballotSearch model with csv search info
+with open('generator/searches.csv') as ballot_searches:
+    db.session.bulk_insert_mappings(BallotSearch, DictReader(ballot_searches))
+
+
+with open('generator/ballots.csv') as ballots:
+    db.session.bulk_insert_mappings(BallotsFromMainDB, DictReader(ballots))
+
+db.session.commit()
