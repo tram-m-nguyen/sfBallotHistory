ed38b5320218a8b80029350f2fd94aa7580b3a5e
tram-m-nguyen <tn334@cornell.edu>
2020-11-19 14:48:45 -0500

    finished query into main database functionality

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 import requests
 from ballots import get_ballots_for_queries
 from models import db, connect_db, BallotSearch, BallotsFromMainDB
 from forms import searchForm
-from helpers import yes_votes, no_votes, display_vote_percents
+from helpers import yesVotes, noVotes, displayVotePercents, isFormEmpty, \
+    canTurnIntoInteger, turnStrIntoSqlQuery, removeEmptyValues
+import copy
+from sqlalchemy import and_
 
+SEARCH_INPUTS = 'searchinputs'
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -39,6 +43,7 @@ def homepage():
     total_ballots = BallotsFromMainDB.query.count()
 
     form = searchForm()
+    print('this is homepagehomepagehomepagehomepagehomepagehomepage', form.data)
 
     # get all ballots from 1907 in the db and return array of objects.
     ballots_1907 = BallotsFromMainDB.query.filter_by(year = 1915) 
@@ -47,38 +52,102 @@ def homepage():
     
     return render_template("index.html", ballots=ballots_1907, year=year,
                                         total_ballots=total_ballots, 
-                                        form=form, yes_votes=yes_votes, 
-                                        no_votes=no_votes,
-                                        display_vote_percents=display_vote_percents)
+                                        form=form, yesVotes=yesVotes, 
+                                        noVotes=noVotes,
+                                        displayVotePercents=displayVotePercents)
 
-# logic for form:
-# if year is provided and it's between 1961-2000
-# then make API call 
 
 # create an api to store search data in database
-@app.route("/search", methods=['POST'])
+@app.route("/search", methods=['GET','POST'])
 def create_ballotsearch():
-    """Add search query, make an api request for 1961, and return data about all
-    ballots in 1961.
+    """Save search into database.
     
     Return JSON: { ballots: [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {
-                                month, year, letter, subject, yes_votes, 
-                                no_votes, pass_or_fail, percent, type_measure, 
+                                month, year, letter, subject, yesVotes, 
+                                noVotes, pass_or_fail, percent, type_measure, 
                                 by, keywork1, keyword2, keyword3, keyword4 or 
                                 keyword5}]}"""
 
+    form = searchForm()
 
-    #get data from the front end as through request.json
-    #create a new ballotsearch instance
-    #add instance into database
-    #commit to database 
+    if isFormEmpty(form.data):
+        return redirect("/")
 
-    #serialized the new cupcake
+    # validate the form, create an instance of search then add into the db, use the 
+    if form.validate_on_submit():
+        ballot = BallotSearch.add_search(
+        month=form.month.data,
+        year=form.year.data,
+        prop_letter=form.prop_letter.data,
+        ballot_subject="" if canTurnIntoInteger(form.ballot_subject.data) else form.ballot_subject.data,
+        type_of_measure=form.type_of_measure.data,
+        measure_placed_on_ballot_by=form.measure_placed_on_ballot_by.data,
+        pass_or_fail=form.pass_or_fail.data,
+        )  
+        
+        db.session.commit()
 
-    return 'should return jsonify'
+        # add search inputs into sessions in order to render results
+        session[SEARCH_INPUTS] = {
+        "month": ballot.month, 
+        "year": ballot.year,
+        "prop_letter": ballot.prop_letter, 
+        "ballot_subject": ballot.ballot_subject,
+        "type_of_measure": ballot.type_of_measure, 
+        "measure_placed_on_ballot_by": ballot.measure_placed_on_ballot_by,
+        "pass_or_fail": ballot.pass_or_fail
+        }
+
+        return redirect("/ballots")
+
+
+    else:
+        return render_template("index.html", form=form)
+
+
+
+@app.route("/ballots", methods=['GET', 'POST'])
+def searchResult():
+    """Get data from form and return the search result."""
+
+    #this is an empty search forms
+    form = searchForm()
+
+    search_inputs = session[SEARCH_INPUTS]
+    print('before search____________________________________________', search_inputs)
     
+    # deep copy the search inputs
+    search_inputs_copy = copy.deepcopy(search_inputs)
+    final_search_inputs = removeEmptyValues(search_inputs_copy)
+    print("final_search_____________________________________________",final_search_inputs)
 
+    # array of filters for SQL    
+    filters = []
+    for key in list(final_search_inputs.keys()):
+        if key == "ballot_subject":
+            sql_subject_str = turnStrIntoSqlQuery(final_search_inputs['ballot_subject'])
+            expression = (BallotsFromMainDB.ballot_subject.like(f'{sql_subject_str}'))
+            print('++++++++++++++++++++++++++++', expression)
+            filters.append(expression)
+        else:
+            expression = (getattr(BallotsFromMainDB, key) == final_search_inputs[key])
+            print('++++++++++++++++++++++++++++', expression)
+            filters.append(expression)
 
+    search_results = db.session.query(BallotsFromMainDB).filter(and_(*filters))
+    print('*************************', db.session.query(BallotsFromMainDB).filter(and_(*filters)))
+    print('*******************  ends')
+
+    year = final_search_inputs['year'] if final_search_inputs['year'] else ""
+    results_total = len(search_results.all())
+    print("TOTAL------------------------", results_total)
+
+    return render_template("result.html", ballots=search_results, year=year,
+                                        results_total= results_total,
+                                        final_search_inputs=final_search_inputs,
+                                        form=form, yesVotes=yesVotes, 
+                                        noVotes=noVotes, search_inputs=search_inputs,
+                                        displayVotePercents=displayVotePercents)
 
 
 
@@ -95,18 +164,18 @@ def create_ballotsearch():
 #     """
 
 #     # get data from the form
-#     inputs_received = request.json
-#     print('this is inputs_received', inputs_received)
+#     search_inputs = request.json
+#     print('this is search_inputs', search_inputs)
 
 #     # create instance of search
 #     ballotSearch = BallotSearch(
-#         year = inputs_received["year"]
-#         month = inputs_received["month"]
-#         subject = inputs_received["subject"]
-#         type_of_measure = inputs_received["type_of_measure"]
-#         measure_placed_by = inputs_received["measure_placed_by"]
-#         pass_or_fail = inputs_received["pass_or_fail"]
-#         keyword = inputs_received["keyword"]
+#         year = search_inputs["year"]
+#         month = search_inputs["month"]
+#         subject = search_inputs["subject"]
+#         type_of_measure = search_inputs["type_of_measure"]
+#         measure_placed_by = search_inputs["measure_placed_by"]
+#         pass_or_fail = search_inputs["pass_or_fail"]
+#         keyword = search_inputs["keyword"]
 #     )
 
 #     # add it to the database
@@ -114,57 +183,3 @@ def create_ballotsearch():
 #     db.session.commit()
 
 #     return jsonify(ballotSearch=ballotSearch.to_dict()), 201)
-
-    
-        
-
-# Adde handles the form submission
-# @app.route("/search-ballots", methods=['GET', 'POST'])
-# def get_ballots():
-#     """ Get data from the front end. Input into db, redirect, and return html."""
-#     print('this is get_ballotsget_ballotsget_ballotsget_ballots') 
-    
-#     #get data from the form
-#     inputs_received = request.json
-#     print('this is inputs_received', inputs_received)
-
-#     #
-#     form = searchForm(data=inputs_received)
-#     print("DATATATATATATAT", inputs_received)
-
-#     # this is get request, so don't need to validate all inputs, just to take 
-#     # error and send to front end
-#     # if post, request, then all inputs must be validated and redirect !!
-#     # get back data fr form, feed it to the get_ballots and get a json because
-#     # that functions gives you a json 
-#     if form.validate_on_submit():
-#         print("validate_on_submitvalidate_on_submitvalidate_on_submit", inputs_received)
-#         year = inputs_received["year"]
-#         # month = inputs_received["month"]
-#         # subject = inputs_received["subject"]
-#         # type_of_measure = inputs_received["type_of_measure"]
-#         # measure_placed_by = inputs_received["measure_placed_by"]
-#         # pass_or_fail = inputs_received["pass_or_fail"]
-#         # keyword = inputs_received["keyword"]
-
-#         print('year input,', year)
-
-#         ballots = get_ballots_for_queries( year=year, 
-#                             # month=month, 
-#                             # subject=subject, 
-#                             # type_of_measure=type_of_measure,
-#                             # by=measure_placed_by,
-#                             # pass_or_fail=pass_or_fail,
-#                             # keyword=keyword
-#                             )
-
-#         print('this is ballotsballotsballotsballots', ballots)
-#         return jsonify(ballots=ballots)
-    
-#     else:
-#         return jsonify(errors=form.errors)
-        
-
-
-
-
